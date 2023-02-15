b94784d51b11c192ea02e5466030e830f1e85e53
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2023-02-15 10:08:52 +0300

    create fileupload model

--- pbp_project/fileupload/models.py
+++ pbp_project/fileupload/models.py
@@ -1,8 +1,46 @@
 from django.db import models
+import enum
+import datetime
 
 
 class FileUpload(models.Model):
-    pass
+    class UploadStatus(enum.Enum):
+        PENDING = 1
+        PROCESSED = 2
+        FAILED = 3
+
+    file_path = models.CharField()
+    uploaded_by = models.ForeignKey("auth.User")
+    upload_date = models.DateTimeField(auto_now_add=True)
+    upload_status = models.IntegerField(
+        choices=UploadStatus, default=UploadStatus.PENDING
+    )
+    processing_message = models.TextField(blank=True, null=True)
+    num_records = models.PositiveIntegerField()
+    start_date_time = models.DateTimeField(null=True)
+    end_date_time = models.DateTimeField(null=True)
+
+    def mark_processed(self, num_records):
+        self.upload_status = self.UploadStatus.PROCESSED
+        self.end_date_time = datetime.datetime.now()
+        self.num_records = num_records
+        self.processing_message = None
+        self.save()
+
+    def mark_failed(self, message):
+        self.upload_status = self.UploadStatus.FAILED
+        self.processing_message = message
+        self.save()
+
+    def upload(self):
+        self.start_date_time = datetime.datetime.now()
+
+        try:
+            pass
+        except Exception as e:
+            self.mark_failed(str(e))
+        else:
+            self.mark_processed(0)
 
 
 class Customer(models.Model):
